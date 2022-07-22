d6f4e4d8feba302be39d85bb1246960254f7f35a
bhagyashreeghude <88839224+bhagyashreeghude@users.noreply.github.com>
2022-07-22 15:02:56 +0530

    Add files via upload

---
+++ project/main.py
@@ -0,0 +1,12 @@
+import time 
+from plyer import notification
+while True:
+    if __name__=="__main__":
+        notification.notify(
+            title="***please drink        waattttttttttttttttter water",
+            message="Water (chemical formula H2O) is an inorganic, transparent, tasteless, odorless, and nearly colorless chemical substance, which is the main constituent of Earth's hydrosphere and the fluids of all known living organisms (in which it acts as a solvent).",
+            app_icon=r"C:\Users\HP\Desktop\apna_college_dsa\icon.ico.ico",
+            timeout=10
+        )
+        time.sleep(60*5)
+    
